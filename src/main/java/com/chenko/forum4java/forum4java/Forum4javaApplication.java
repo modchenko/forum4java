f29ddad47aa466461ea4a4158d6b612807a3b44d
modchenko <modchenko@163.com>
2022-04-13 10:42:45 +0800

    init repo

---
+++ src/main/java/com/chenko/forum4java/forum4java/Forum4javaApplication.java
@@ -0,0 +1,13 @@
+package com.chenko.forum4java.forum4java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Forum4javaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Forum4javaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chenko/forum4java/forum4java/Forum4javaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chenko.forum4java.forum4java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Forum4javaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
